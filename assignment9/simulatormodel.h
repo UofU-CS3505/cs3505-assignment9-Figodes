c7aa0adf832870db007812e654321cf810c2cc73
CobaltE27 <potassiume19@gmail.com>
2024-04-10 12:54:01 -0600

    Began model implementation

--- assignment9/simulatormodel.h
+++ assignment9/simulatormodel.h
@@ -1,15 +1,23 @@
 #ifndef SIMULATORMODEL_H
 #define SIMULATORMODEL_H
 
-#include "uilogicgate.h"
+#include <QVector>
 
 class simulatorModel
 {
+private:
+    class gateNode
+    {
+    public:
+        gateNode(qint32 inputCount, qint32 outputCount, std::function<void()> evaluatorFunc);
+        QVector<gateNode*> inputFromNodes;
+        QVector<gateNode*> outputToNodes;
+        QVector<bool> inputStates;
+        QVector<bool> outputStates;
+        std::function<void()> evaluate;
+    };
 public:
     simulatorModel();
-
-private:
-    QVector <UILogicGate> gates;
 };
 
 #endif // SIMULATORMODEL_H

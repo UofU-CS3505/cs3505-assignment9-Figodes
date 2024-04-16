a00316ef7b08030afc959ea0af3b4cb5cf09b092
CobaltE27 <potassiume19@gmail.com>
2024-04-15 18:18:48 -0600

    Created Level constructor that takes a functor to generate outputs

--- assignment9/level.cpp
+++ assignment9/level.cpp
@@ -1,5 +1,6 @@
 #include "level.h"
 #include <iostream>
+#include <QtMath>
 
 Level::Level(QString description, QVector<QVector<bool>> expectedOutputs, qint32 inputCount, qint32 outputCount)
     : inputCount(inputCount)
@@ -7,7 +8,42 @@ Level::Level(QString description, QVector<QVector<bool>> expectedOutputs, qint32
     , description(description)
     , expectedOutputs(expectedOutputs)
 {
+    for (qint32 n = 0; n < qPow(2, inputCount); n++)
+    {
+        QVector<bool> inputGroup;
+        for(int i = 0; i < inputCount; i++){
+            bool bit = (n >> i) & 1; //get ith bit of integer
+            inputGroup.append(bit);
+        }
+        expectedInputs.append(inputGroup);
+    }
+}
 
+Level::Level(QString description, std::function<void(QVector<bool> , QVector<bool>&)> levelFunction, qint32 inputCount, qint32 outputCount)
+    : inputCount(inputCount)
+    , outputCount(outputCount)
+    , description(description)
+{
+    qint32 numberOfInputSets = qPow(2, inputCount);
+    for (qint32 n = 0; n < numberOfInputSets; n++)
+    {
+        QVector<bool> inputGroup;
+        for(int i = 0; i < inputCount; i++){
+            bool bit = (n >> i) & 1; //get ith bit of integer
+            inputGroup.append(bit);
+        }
+        expectedInputs.append(inputGroup);
+    }
+
+    for (qint32 i = 0; i < numberOfInputSets; i++) //initialize outputs
+    {
+        expectedOutputs.append(QVector<bool>());
+        for (int j = 0; j < outputCount; j++)
+            expectedOutputs[i].append(false);
+    }
+
+    for (qint32 i = 0; i < numberOfInputSets; i++)
+        levelFunction(expectedInputs[i], expectedOutputs[i]); //generate appropriate outputs
 }
 
 QString Level::getDescription(){
--- assignment9/level.h
+++ assignment9/level.h
@@ -8,14 +8,17 @@ class Level
 {
 public:
     Level(QString description, QVector<QVector<bool>> expectedOutputs, qint32 inputCount, qint32 outputCount);
+    Level(QString description, std::function<void(QVector<bool> , QVector<bool>&)> levelFunction, qint32 inputCount, qint32 outputCount);
     qint32 inputCount;
     qint32 outputCount;
     QString getDescription();
-    QVector<bool> getExpectedOutput(qint32 input);
+    QVector<bool> getExpectedOutput(qint32 inputIndex);
+    QVector<bool> getExpectedInput(qint32 inputIndex);
 
 private:
     QString description;
     QVector<QVector<bool>> expectedOutputs;
+    QVector<QVector<bool>> expectedInputs;
 };
 
 #endif // LEVEL_H

b86570f298b964021accd9cbaa6b2afdda5d403b
tyzawesome <tyzawesome1@gmail.com>
2024-04-15 20:01:39 -0600

    added stylesheet

--- assignment9/main.cpp
+++ assignment9/main.cpp
@@ -2,6 +2,8 @@
 
 #include <QApplication>
 #include "uilogicgate.h"
+#include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +11,17 @@ int main(int argc, char *argv[])
     MainWindow w;
 
     //TODO: when game is finished, delete below line. closing the welcome screen will make the game appear
+    QFile styleSheetFile(":/Adaptic/Adaptic.qss");
+    if (!styleSheetFile.open(QFile::ReadOnly)) {
+        qDebug() << "Failed to open stylesheet file";
+        return 1;  // Exit or handle the error as appropriate
+    }
+    styleSheetFile.open(QFile::ReadOnly);
+
+    QString styleSheet { QLatin1String(styleSheetFile.readAll()) };
+
+    //setup stylesheet
+    a.setStyleSheet(styleSheet);
     w.show();
     return a.exec();
 }
